c6934149601ab2b37718e03a55921fc3b359f95a
amit-patra <mcaamit099@gmail.com>
2025-10-28 21:41:39 +0530

    basic_practice
    basic_practice

---
+++ Programming 101/basic_code_practice.js
@@ -0,0 +1,38 @@
+let firstName = "Amit";
+let lastName = "Patra";
+// firstName = 7;
+let fullName = firstName + " "+ lastName;
+// console.log(fullName);
+
+let arr = ["Amit", "Manvik", "Dipa", [5,10,[6,7]]];
+
+// console.log(arr[3][2][0]);
+
+// Object
+
+let obj = {
+    a: 1,
+    b: "Amit",
+    c: [10,20],
+    d: false
+}
+
+// console.log(obj.c);
+
+function elegibleToVote(age){
+    if(age<18){
+        console.log("Not Elegible for vote");
+        
+    } else{
+        console.log("Elegible");
+        
+    }
+}
+
+elegibleToVote(10);
+
+
+
+
+
+
