fc7849574983b4a52482f697cf88a51bf0317b61
amit-patra <mcaamit099@gmail.com>
2026-01-12 22:17:05 +0530

    Add Insertion sort

--- 31-Dec-2025 (Start)/Sorting_Programs/insertion_sort.js
+++ 31-Dec-2025 (Start)/Sorting_Programs/insertion_sort.js
@@ -3,3 +3,21 @@
  * Input: [5, 1, 3, 2]
  * Output: [1, 2, 3, 5]
  */
+
+let arr = [7, 4, 3, 5, 1, 2];
+
+function insertionSort(a) {
+  let n = a.length;
+  for (let i = 1; i < n; i++) {
+    let curr = a[i];
+    let prev = i - 1;
+    while (a[prev] > curr && prev >= 0) {
+      a[prev + 1] = a[prev];
+      prev--;
+    }
+    a[prev + 1] = curr;
+  }
+  return a;
+}
+
+console.log(insertionSort(arr));
