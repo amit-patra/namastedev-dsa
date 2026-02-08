e3df8f3ff6c4b4d0924b18285396ac488a03e3e6
Amit Patra <mcaamit099@gmail.com>
2026-02-08 08:21:17 +0530

    Add stub: most frequent vowel & consonant
    Add a new JavaScript file containing the problem statement, LeetCode URL, and examples for "Find Most Frequent Vowel and Consonant". This is a problem stub (no implementation yet) intended as the starting point for implementing the solution and documenting input/output examples.

---
+++ 31-Dec-2025 (Start)/Strings/find_most_frequent_vowel_and_consonant.js
@@ -0,0 +1,24 @@
+/**
+ *  Find Most Frequent Vowel and Consonant
+    * URL: https://leetcode.com/problems/find-most-frequent-vowel-and-consonant/description/
+
+Example 1:
+Input: s = "successes"
+Output: 6
+
+Explanation:
+
+The vowels are: 'u' (frequency 1), 'e' (frequency 2). The maximum frequency is 2.
+The consonants are: 's' (frequency 4), 'c' (frequency 2). The maximum frequency is 4.
+The output is 2 + 4 = 6.
+
+Example 2:
+Input: s = "aeiaeia"
+Output: 3
+
+Explanation:
+The vowels are: 'a' (frequency 3), 'e' ( frequency 2), 'i' (frequency 2). The maximum frequency is 3.
+There are no consonants in s. Hence, maximum consonant frequency = 0.
+The output is 3 + 0 = 3.
+ */
+
