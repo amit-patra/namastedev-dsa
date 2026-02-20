df073109b7d4ed7ef9f5b36b5a4caa19fb054a79
amit-patra <mcaamit099@gmail.com>
2026-02-20 06:06:56 +0530

    Add Balanced String Split solution
    Add JavaScript solution for LeetCode "Split a String in Balanced Strings" (31-Dec-2025/Strings). Includes an initial approach counting L and R separately and a simpler, preferred approach using a single balance counter. Time complexity O(n), space complexity O(1).

---
+++ 31-Dec-2025 (Start)/Strings/split_a_string_in_balanced_strings.js
@@ -0,0 +1,57 @@
+/**
+ * Split a String in Balanced Strings
+ * URL: https://leetcode.com/problems/split-a-string-in-balanced-strings/description/
+ * 
+Example 1:
+Input: s = "RLRRLLRLRL"
+Output: 4
+Explanation: s can be split into "RL", "RRLL", "RL", "RL", each substring contains same number of 'L' and 'R'.
+
+Example 2:
+Input: s = "RLRRRLLRLL"
+Output: 2
+Explanation: s can be split into "RL", "RRRLLRLL", each substring contains same number of 'L' and 'R'.
+Note that s cannot be split into "RL", "RR", "RL", "LR", "LL", because the 2nd and 5th substrings are not balanced.
+
+Example 3:
+Input: s = "LLLLRRRR"
+Output: 1
+Explanation: s can be split into "LLLLRRRR".
+*/
+
+// Aproach -1
+var balancedStringSplit = function (s) {
+  let r = 0;
+  let l = 0;
+  let count = 0;
+  for (let ch of s) {
+    if (ch == "L") {
+      l++;
+    } else {
+      r++;
+    }
+    if (l == r) {
+      l = r = 0;
+      count++;
+    }
+  }
+  return count;
+};
+
+// Best Aproach
+
+var balancedStringSplit = function (s) {
+  let balance = 0;
+  let count = 0;
+  for (let ch of s) {
+    balance += ch === "L" ? 1 : -1;
+    if (balance == 0) {
+      count++;
+    }
+  }
+  return count;
+};
+/**
+ * Time Complexity O(n)
+ * Space Complexity: O(1)
+ */
